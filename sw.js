d89b8014d84a3cb34da341433e6523c7812422e1
Gabriel G Costa <negociolocal@twodreamers.com.br>
2026-07-18 11:53:42 -0300

    chore: bump cache v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vofatima-v19';
+const CACHE = 'vofatima-v20';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
